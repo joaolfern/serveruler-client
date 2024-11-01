30530ed75b098f441dcf2261d1c8c2214a7b5e2c
João Fernandes <joaolfern@proton.me>
2024-10-31 23:17:25 -0300

    feat: add github link

--- src/layout/components/Header.tsx
+++ src/layout/components/Header.tsx
@@ -2,8 +2,14 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-
+import { Chip, } from '@mui/material'
+import GitHub from '@mui/icons-material/GitHub';
 export function Header() {
+
+  const openGithub = () => {
+    window.open('https://github.com/joaolfern/severuler-client', '_blank');
+  };
+
   return (
     <AppBar position="static">
       <Container maxWidth="lg">
@@ -17,18 +23,17 @@ export function Header() {
           <Typography
             variant="h5"
             noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
+            component="p"
             sx={{
               mr: 2,
               flexGrow: 1,
               fontWeight: 700,
-              textDecoration: "none",
               color: "inherit",
             }}
           >
             Serveruler
           </Typography>
+          <Chip icon={<GitHub  style={{ fontSize: 14, marginLeft: '6px'}} />} label="Github" size='small' variant="outlined"  onClick={() => openGithub()}/>
         </Toolbar>
       </Container>
     </AppBar>
