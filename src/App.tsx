8cbe4839abfbf62ce87b0e1df52f82d3a3d57c3f
João Fernandes <joaolfern@proton.me>
2024-11-04 10:34:05 -0300

    refactor: remove unused redirect code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { Suspense, lazy, useLayoutEffect } from "react";
+import { Suspense, lazy } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import CircularProgress from "@mui/material/CircularProgress";
-import { getIsCompanyDay } from "./utils/getIsCompanyDay";
 import { Layout } from "./layout/Layout";
 import { appTheme } from "./theme/appTheme";
 
 const Serveruler = lazy(() => import("./Serveruler"));
-const isProduction = import.meta.env.VITE_IS_PRODUCTION;
 
 const theme =
   window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches
@@ -16,37 +14,19 @@ const theme =
     : "light";
 
 function App() {
-  useLayoutEffect(() => {
-    if (isProduction) redirectToLocalNetwork();
-  }, [isProduction]);
-
   return (
     <ThemeProvider theme={appTheme(theme)}>
       <CssBaseline />
       <Layout>
-        {isProduction ? (
-          <Loader />
-        ) : (
-          <Suspense fallback={<Loader />}>
-            <Serveruler />
-          </Suspense>
-        )}
+        <Suspense fallback={<Loader />}>
+          <Serveruler />
+        </Suspense>
       </Layout>
     </ThemeProvider>
   );
 }
 
 export default App;
-
-function redirectToLocalNetwork() {
-  const isCompanyDay = getIsCompanyDay();
-  const ipAddress = isCompanyDay ? COMPANY_IP_ADDRESS : HOME_IP_ADDRESS;
-
-  window.location.href = ipAddress;
-}
-
-const COMPANY_IP_ADDRESS = "http://10.10.0.197:5173/";
-const HOME_IP_ADDRESS = "http://172.24.196.14:5173/";
 
 const Loader = () => {
   return (
