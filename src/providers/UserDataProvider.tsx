a4c77ee44a82d415d1244cf8eee10be8045c6d35
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-10-01 00:32:23 -0300

    feat: extract getAvailableEnvs and getInitialEnvOptionIndex functions into separate utility files

--- src/providers/UserDataProvider.tsx
+++ src/providers/UserDataProvider.tsx
@@ -1,7 +1,9 @@
 import { ReactNode, createContext, useEffect, useState } from 'react'
-import { COMPANY_DAYS, SERVER_OPTIONS } from '../constants'
+import { SERVER_OPTIONS } from '../constants'
+import { getAvailableEnvs } from '../utils/getAvailableEnvs'
+import { getInitialEnvOptionIndex } from '../utils/getInitialEnvOptionIndex'
 
-type Data = Record<string, Record<string, string>>
+export type Data = Record<string, Record<string, string>>
 
 interface UserDataContextProps {
   data: Data
@@ -38,7 +40,6 @@ export function UserDataProvider({ children }: { children: ReactNode }) {
 
   useEffect(() => {
     if (!data) return
-
     const envOptions = getAvailableEnvs(data)
     setEnvOptions(envOptions)
   }, [data])
@@ -69,22 +70,4 @@ async function fetchData() {
   const response = await fetch('data.json')
   const data = await response.json()
   return data
-}
-
-function getAvailableEnvs(data: Data): string[] {
-  const envs: string[] = []
-  for (const user in data) {
-    for (const env in data[user]) {
-      const isNewEnv = !envs.includes(env)
-      if (isNewEnv) envs.push(env)
-    }
-  }
-  return envs
-}
-
-function getInitialEnvOptionIndex() {
-  const weekDay = new Date().getDay()
-  const isCompanyDay = COMPANY_DAYS.includes(weekDay)
-  const initialOptionIndex = isCompanyDay ? 0 : 1
-  return initialOptionIndex
 }
